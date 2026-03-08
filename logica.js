887a00712d05d62cb946fc8b1e7b6d191f165495
juanpabloramirez180803-design <juanpabloramirez180803@gmail.com.com>
2026-03-08 16:36:50 -0300

    sidebar

--- logica.js
+++
@@ -1,6 +0,0 @@
-const boton = document.getElementById("btn");
-const mensaje = document.getElementById("mensaje");
-
-boton.addEventListener("click", () => {
-    mensaje.textContent = "🚀 JavaScript funcionando correctamente en la nube!";
-});
